ad5db596dff0791d81a71d0ea2d93a169a660975
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-29 01:57:48 +0800

    Update

--- assets/js/set-footer.js
+++ assets/js/set-footer.js
@@ -1,11 +1,11 @@
 // set-footer.js
 
-function getCurrentYear() {
-    var currentDate = new Date();
-    return currentDate.getFullYear();
-};
+// function getCurrentYear() {
+//     var currentDate = new Date();
+//     return currentDate.getFullYear();
+// };
 
-$.getJSON('visualize/data/footer-data.json', function(data) {var footer_data = data;});
+$.getJSON('json/footer-data.json', function(data) {var footer_data = data;});
 $('#footer-info').append($('<p>').append($('<a>').attr('href', footer_data.site.link).html(footer_data.site.name)).append(' (' + footer_data.site.version + ')'));
 $('#footer-info').append($('<p>').attr('id', 'powered-by').html('Powered by '));
 for (var i in footer_data.powered_by) {
@@ -17,9 +17,9 @@ for (var i in footer_data.powered_by) {
         $('#powered-by').append(' &middot; ').append($('<a>').attr('href', powered_by.link).attr('target', '_blank').html(powered_by.name)).append(' ' + powered_by.version);
     }
 }
-$('#footer-info').append($('<p>').html('&copy; ' + getCurrentYear() + ' ')
-    .append(footer_data.copyright.organization.prefix)
-    .append($('<a>').attr('href', footer_data.copyright.organization.link).attr('target', '_blank').html(footer_data.copyright.organization.name))
-    .append(footer_data.copyright.organization.suffix)
+$('#footer-info').append($('<p>').html('&copy; ' + function(){var currentDate = new Date(); return currentDate.getFullYear();} + ' ')
+    .append(footer_data.organization.prefix)
+    .append($('<a>').attr('href', footer_data.organization.link).attr('target', '_blank').html(footer_data.organization.name))
+    .append(footer_data.organization.suffix)
 );
 $('#footer-info').append($('<p>').attr('id', 'contact').append($('<i>').attr('aria-hidden', 'true').addClass('fa fa-paper-plane-o')).append(' Contact: ').append($('<a>').attr('href', 'mailto:' + footer_data.site.support + '?subject=Mail From Y-Lab Website').html(footer_data.site.support)));
