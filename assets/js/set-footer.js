5cb5b5cbd1ff878af263e6e4cec459bea8037a49
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-29 02:16:35 +0800

    Update

--- assets/js/set-footer.js
+++ assets/js/set-footer.js
@@ -5,11 +5,11 @@
 //     return currentDate.getFullYear();
 // };
 
-$.getJSON('./json/footer-data.json', function(footer_data) {
-    $('#footer-info').append($('<p>').append($('<a>').attr('href', footer_data.site.link).html(footer_data.site.name)).append(' (' + footer_data.site.version + ')'));
+$.getJSON('./json/footer-data.json', function(data) {
+    $('#footer-info').append($('<p>').append($('<a>').attr('href', data.site.link).html(data.site.name)).append(' (' + data.site.version + ')'));
     $('#footer-info').append($('<p>').attr('id', 'powered-by').html('Powered by '));
-    for (var i in footer_data.powered_by) {
-        powered_by = footer_data.powered_by[i]
+    for (var i in data.powered_by) {
+        powered_by = data.powered_by[i]
         if (i < 1) {
             $('#powered-by').append($('<a>').attr('href', powered_by.link).attr('target', '_blank').html(powered_by.name)).append(' ' + powered_by.version);
         }
@@ -17,10 +17,10 @@ $.getJSON('./json/footer-data.json', function(footer_data) {
             $('#powered-by').append(' &middot; ').append($('<a>').attr('href', powered_by.link).attr('target', '_blank').html(powered_by.name)).append(' ' + powered_by.version);
         }
     }
-    $('#footer-info').append($('<p>').html('&copy; ' + function(){var currentDate = new Date(); return currentDate.getFullYear();} + ' ')
-        .append(footer_data.organization.prefix)
-        .append($('<a>').attr('href', footer_data.organization.link).attr('target', '_blank').html(footer_data.organization.name))
-        .append(footer_data.organization.suffix)
+    $('#footer-info').append($('<p>').html('&copy; ' + function(var currentDate = new Date()){return currentDate.getFullYear();} + ' ')
+        .append(data.organization.prefix)
+        .append($('<a>').attr('href', data.organization.link).attr('target', '_blank').html(data.organization.name))
+        .append(data.organization.suffix)
     );
-    $('#footer-info').append($('<p>').attr('id', 'contact').append($('<i>').attr('aria-hidden', 'true').addClass('fa fa-paper-plane-o')).append(' Contact: ').append($('<a>').attr('href', 'mailto:' + footer_data.site.support + '?subject=Mail From Y-Lab Website').html(footer_data.site.support)));
+    $('#footer-info').append($('<p>').attr('id', 'contact').append($('<i>').attr('aria-hidden', 'true').addClass('fa fa-paper-plane-o')).append(' Contact: ').append($('<a>').attr('href', 'mailto:' + data.site.support + '?subject=Mail From Y-Lab Website').html(data.site.support)));
 });
