4294b8b38e19c7dd41911f0480462b43a936ddb3
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-29 02:19:55 +0800

    Update

--- assets/js/set-footer.js
+++ assets/js/set-footer.js
@@ -1,22 +1,16 @@
 // set-footer.js
 
-// function getCurrentYear() {
-//     var currentDate = new Date();
-//     return currentDate.getFullYear();
-// };
-
 $.getJSON('./json/footer-data.json', function(data) {
     $('#footer-info').append($('<p>').append($('<a>').attr('href', data.site.link).html(data.site.name)).append(' (' + data.site.version + ')'));
     $('#footer-info').append($('<p>').attr('id', 'powered-by').html('Powered by '));
     for (var i in data.powered_by) {
-        powered_by = data.powered_by[i]
         if (i < 1) {
-            $('#powered-by').append($('<a>').attr('href', powered_by.link).attr('target', '_blank').html(powered_by.name)).append(' ' + powered_by.version);
+            $('#powered-by').append($('<a>').attr('href', data.powered_by[i].link).attr('target', '_blank').html(data.powered_by[i].name)).append(' ' + data.powered_by[i].version);
         }
         else {
-            $('#powered-by').append(' &middot; ').append($('<a>').attr('href', powered_by.link).attr('target', '_blank').html(powered_by.name)).append(' ' + powered_by.version);
-        }
-    }
+            $('#powered-by').append(' &middot; ').append($('<a>').attr('href', data.powered_by[i].link).attr('target', '_blank').html(data.powered_by[i].name)).append(' ' + data.powered_by[i].version);
+        };
+    };
     $('#footer-info').append($('<p>').html(function(){var currentDate = new Date(); return '&copy; ' +  currentDate.getFullYear() + ' ';})
         .append(data.organization.prefix)
         .append($('<a>').attr('href', data.organization.link).attr('target', '_blank').html(data.organization.name))
