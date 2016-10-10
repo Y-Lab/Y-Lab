542502d7c44ee3951524faa47ab1d2d7e160d2b9
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-10-10 16:09:03 +0800

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -7,4 +7,4 @@ $.getJSON('../json/site-info.json', function(data) {
 var lastCommitTime = '2016-05-31T00:00:00';
 var version = moment(lastCommitTime, 'YYYY-MM-DDTh:mm:ss').utcOffset(8).fromNow();
 
-$('#dev-log-version').append($('<i>').addClass('fa fa-spinner fa-pulse color-dark-gray').attr('aria-hidden', 'true')).append(' Updated: ' + version);
+$('#dev-log-version').append($('<i>').addClass('fa fa-spinner fa-pulse color-dark-gray').attr('aria-hidden', 'true')).append(' Updated ' + version);
