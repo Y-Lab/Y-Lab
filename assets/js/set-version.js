61f62bb7cd06972af47c093ef87b4a4128774467
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-10-27 15:36:35 +0800

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -4,6 +4,10 @@ $.getJSON('http://lab.y-english.org/json/site-info.json', function(data) {
     $('#site-version').append($('<i>').addClass('fa fa-globe').attr('aria-hidden', 'true')).append(' ' + data.site.version);
 });
 
-$.getJSON('https://api.github.com/repos/Y-Lab/Y-Lab.github.io', function(data) {
-    $('#dev-log-version').append($('<i>').addClass('fa fa-refresh fa-spin fa-fw color-dark-gray').attr('aria-hidden', 'true')).append(' Updated ' + moment(data.pushed_at, 'YYYY-MM-DDTh:mm:ssZ').fromNow());
-});
+function updateVersion() {
+    $.getJSON('https://api.github.com/repos/Y-Lab/Y-Lab.github.io', function(data) {
+        $('#dev-log-version').html('<i class="fa fa-refresh fa-spin fa-fw color-dark-gray" aria-hidden="true"></i> Updated ' + moment(data.pushed_at, 'YYYY-MM-DDTh:mm:ssZ').fromNow());
+    });
+};
+updateVersion();
+setInterval(updateVersion(), 15000);
