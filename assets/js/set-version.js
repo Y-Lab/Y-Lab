a4c47701d8be8bf3a6cdee6236b59de71e86e597
iRB Air <irockbunny@outlook.com>
2017-03-26 22:01:57 +0800

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -1,15 +1,10 @@
 // set-dev-version.js
 
-var pushedAt = 'loading';
-$.getJSON('https://api.github.com/repos/Y-Lab/Y-Lab.github.io', function(data) {
-    pushedAt = data.pushed_at;
-});
 function updateVersion(timestamp) {
-    if (timestamp !== 'loading') {
-        $('#dev-log-version').html('<i class="fa fa-refresh fa-spin fa-fw color-dark-gray" aria-hidden="true"></i>Updated ' + moment(timestamp).fromNow());
-    };
+    $('#dev-log-version').html('<i class="fa fa-refresh fa-spin fa-fw color-dark-gray" aria-hidden="true"></i>Updated ' + moment(timestamp).fromNow());
 };
-updateVersion(pushedAt);
+var updateAt = $('#dev-log-version').attr('data-update-at');
+updateVersion(updateAt);
 setInterval(function () {
-    updateVersion(pushedAt)
+    updateVersion(updateAt)
 }, 15000);
