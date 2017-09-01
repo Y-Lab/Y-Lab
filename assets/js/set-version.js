b7329689ac071c1ac3e835b05536d0b965fddaf0
iROCKBUNNY <irockbunny@outlook.com>
2017-09-01 01:09:13 +0100

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -3,7 +3,7 @@
 function updateVersion(timestamp) {
     $('#dev-log-version').html('<i class="fa fa-refresh fa-spin fa-fw color-dark-gray" aria-hidden="true"></i>Updated ' + moment(timestamp).fromNow());
 };
-var updateAt = $('#dev-log-version').attr('data-update-at');
+var updateAt = $('meta[name=updated_at]').attr('content');
 updateVersion(updateAt);
 setInterval(function () {
     updateVersion(updateAt)
