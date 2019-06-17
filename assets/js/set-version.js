b9317c517fa205c26f6bae729b0356f652c687b9
iROCKBUNNY <irockbunny@outlook.com>
2019-06-18 02:41:06 +0800

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -2,7 +2,7 @@
 
 dayjs.extend(dayjs_plugin_relativeTime);
 function updateVersion(timestamp) {
-    $('#dev-log-version').html('<i class="fa fa-refresh fa-spin fa-fw color-dark-gray" aria-hidden="true"></i>Updated ' + dayjs(timestamp).fromNow());
+    $('#dev-log-version').html(`<i class="fas fa-sync-alt fa-spin fa-fw color-dark-gray" aria-hidden="true"></i>Updated ${dayjs(timestamp).fromNow()}`);
 };
 var updateAt = $('meta[name=updated_at]').attr('content');
 updateVersion(updateAt);
