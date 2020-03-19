dc5d58debaface7f41c7fd707dbc306923ab2af0
iROCKBUNNY <irockbunny@outlook.com>
2020-03-19 10:19:38 +0800

    Update

--- assets/js/set-version.js
+++ assets/js/set-version.js
@@ -1,6 +1,6 @@
 // set-version.js
 
-dayjs.extend(dayjs_plugin_relativeTime);
+dayjs.extend(window.dayjs_plugin_relativeTime);
 function updateVersion(timestamp) {
     $('#dev-log-version').html(`<i class="fas fa-sync-alt fa-spin fa-fw color-dark-gray" aria-hidden="true"></i>Updated ${dayjs(timestamp).fromNow()}`);
 };
